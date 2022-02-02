36c51a97a43b90862c65e50cfcb63d3c549f5455
unknown <agustindavid@gmail.com>
2022-02-02 00:00:42 -0600

    netlify5

--- src/stores/posts.js
+++ src/stores/posts.js
@@ -29,7 +29,7 @@ query getPosts {
 export const fetchPosts = async () => {
     if (loaded) return;
 
-      const response = await fetch(import.meta.env.VITE_PUBLIC_WORDPRESS_API_URL, {
+      const response = await fetch("https://mdsmx.xyz/fuga/graphql", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
