39dbe16ad745a9721cef797a85ec0988321af649
unknown <agustindavid@gmail.com>
2022-02-01 18:36:32 -0600

    cloudfare

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,6 @@
 import { isoImport } from "vite-plugin-iso-import";
+import adapter from '@sveltejs/adapter-static';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,6 +9,8 @@ const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
+    adapter: adapter(),
+
 
     vite: {
       plugins: [isoImport()],
