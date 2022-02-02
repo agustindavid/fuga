feabb275d6e2f178153efe68c35964c1a9a7ff42
unknown <agustindavid@gmail.com>
2022-02-02 00:05:19 -0600

    netlify7

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { isoImport } from "vite-plugin-iso-import";
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 
 
 /** @type {import('@sveltejs/kit').Config} */
