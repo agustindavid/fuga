35e3b1919d0a458849e347254c6c417444f07932
unknown <agustindavid@gmail.com>
2022-02-02 00:26:21 -0600

    parecequesi

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -19,7 +19,7 @@
             }
             `;
 
-         const response = await fetch(import.meta.env.VITE_PUBLIC_WORDPRESS_API_URL, {
+         const response = await fetch("https://mdsmx.xyz/fuga/graphql", {
            method: 'POST',
            headers: {
              'Content-Type': 'application/json',
