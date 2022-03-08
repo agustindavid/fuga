bf9ff5a10d27302e07da5969e0d9360435c47338
unknown <agustindavid@gmail.com>
2022-03-08 01:14:59 -0600

    vimeo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -129,7 +129,12 @@
 
     <div class=" bg-gray-50 ">  
       <div class="container mx-auto py-12 md:px-16 px-8">
-        <svelte:component this={VideoPlayer} poster={videoPoster} source={videoHome} />
+        <!-- <svelte:component this={VideoPlayer} poster={videoPoster} source={videoHome} /> -->
+        <div style="padding:56.25% 0 0 0;position:relative;">
+          <iframe src="https://player.vimeo.com/video/685761108?h=afbad10175" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen>
+          </iframe>
+        </div>
+        <script src="https://player.vimeo.com/api/player.js"></script>
       </div>
     </div>
 
